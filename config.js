8804ad738fb7a70e65babead5d6363fe61c7752b
Fan <fan@choerodon.io>
2018-08-16 15:38:39 +0800

    [FIX] fix dashboard route

--- config.js
+++ config.js
@@ -3,15 +3,15 @@ const config = {
     clientId: 'localhost', // 必须填入响应的客户端（本地开发）
     titlename: 'Choerodon', //项目页面的title名称
     favicon: 'favicon.ico', //项目页面的icon图片名称
-    fileServer: 'http://minio.staging.saas.hand-china.com',
+    fileServer: 'http://minio.choerodon.com.cn',
     theme: {
         'primary-color': '#3F51B5',
     },
     cookieServer: '', //子域名token共享
     server: 'http://api.choerodon.com.cn',
     dashboard: {
-        iam: 'src/app/iam/dashboard/*',
-        testManager: 'src/app/testManager/dashboard/*'
+        iam: 'choerodon-front-iam/iam/src/app/iam/dashboard/*',
+        testManager: 'choerodon-front-test-manager/testManager/src/app/testManager/dashboard/*'
     },
 };
 
