c6ac8a4404b3a6e7c976651aa0d19ae8611ad413
Fan <fan@choerodon.io>
2018-09-26 16:33:59 +0800

    [IMP] update config.js

--- config.js
+++ config.js
@@ -3,12 +3,13 @@ const config = {
     clientId: 'localhost', // 必须填入响应的客户端（本地开发）
     titlename: 'Choerodon', //项目页面的title名称
     favicon: 'favicon.ico', //项目页面的icon图片名称
-    fileServer: 'http://minio.choerodon.com.cn',
+    fileServer: 'http://minio.choerodon.example.com',
     theme: {
         'primary-color': '#3F51B5',
     },
     cookieServer: '', //子域名token共享
-    server: 'http://api.choerodon.com.cn',
+    server: 'http://api.choerodon.example.com',
+    webSocketServer: 'ws://websocket.choerodon.example.com',
     dashboard: {
         iam: 'choerodon-front-iam/iam/src/app/iam/dashboard/*',
         testManager: 'choerodon-front-test-manager/testManager/src/app/testManager/dashboard/*',
