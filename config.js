f6aeb64d6d369511e495a55445b7af42625c7fa4
陈彬杰 <binjie.chen@hand-china.com>
2018-11-14 10:22:25 +0800

    [IMP] change titlename & update choerodon-front-boot

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   local: true, //是否为本地开发
   clientId: 'localhost', // 必须填入响应的客户端（本地开发）
-  titlename: 'Choerodon', //项目页面的title名称
+  titlename: 'Choerodon | 企业数字化服务平台', //项目页面的title名称
   favicon: 'favicon.ico', //项目页面的icon图片名称
   fileServer: 'http://minio.choerodon.example.com',
   theme: {
