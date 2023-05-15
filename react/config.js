f7abd366c80856683725132b3ad9d0d6e5cda54a
郑皓天 <273070111@qq.com>
2023-05-15 10:07:32 +0800

    [ADD]修改package

--- react/config.js
+++ react/config.js
@@ -1,18 +1,18 @@
 const config = {
   local: true, //是否为本地开发
   modules: [
-    '@choerodon/devops',
-    '@choerodon/testmanager',
-    '@choerodon/agile',
+    '@choerodon/workflow',
     '@choerodon/knowledge',
+    '@choerodon/waterfall',
     '@choerodon/asgard',
-    '@choerodon/notify',
+    '@choerodon/devops',
+    '@choerodon/base-business',
     '@choerodon/manager',
+    '@choerodon/agile',
+    '@choerodon/notify',
+    '@choerodon/testmanager',
     '@choerodon/code-repo',
-    '@choerodon/doc-repo',
     '@choerodon/prod-repo',
-    "@choerodon/hrds-qa",
-    "@choerodon/base-business"
   ]
 };
 
